4efbfe1a6050a29426d7f662ef6d8d55469aab04
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-05-03 12:02:16 +0100

    Populating UpdateWaterContent activity.

--- app/src/main/java/assessment/c1714546/c1714546assessment/updateWaterContent/UpdateWaterContentActivity.java
+++ app/src/main/java/assessment/c1714546/c1714546assessment/updateWaterContent/UpdateWaterContentActivity.java
@@ -1,7 +1,14 @@
 package assessment.c1714546.c1714546assessment.updateWaterContent;
 
+import android.annotation.TargetApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import assessment.c1714546.c1714546assessment.R;
 
@@ -11,5 +18,20 @@ public class UpdateWaterContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_water_content);
+
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        AppCompatTextView genTime = (AppCompatTextView)findViewById(R.id.gen_time);
+        generateTime(genTime);
+    }
+
+    // The below annotation rule should mean
+    // this method works for devices using API level 26+.
+    @TargetApi(26)
+    public void generateTime(AppCompatTextView genTime) {
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter formattedTime = DateTimeFormatter.ofPattern("HH:mm:ss a");
+        genTime.setText("Time:" + currentTime.format(formattedTime));
     }
 }
