5aa281bc575ebb1e6a83e19254b029cfd03010f4
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-05-03 10:23:06 +0100

    Inital commit

---
+++ app/src/main/java/assessment/c1714546/c1714546assessment/HomeActivity.java
@@ -0,0 +1,13 @@
+package assessment.c1714546.c1714546assessment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
