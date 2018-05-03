e44f0fe3f4c2b4d62bb84f0afbbe72a9bbcff340
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-05-03 10:40:32 +0100

    Can launch UpdateWaterContent activity.

--- app/src/main/java/assessment/c1714546/c1714546assessment/HomeActivity.java
+++ app/src/main/java/assessment/c1714546/c1714546assessment/HomeActivity.java
@@ -1,13 +1,28 @@
 package assessment.c1714546.c1714546assessment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class HomeActivity extends AppCompatActivity {
+import assessment.c1714546.c1714546assessment.waterContent.UpdateWaterContentActivity;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView launchUpdateWaterContent = (ImageView)findViewById(R.id.water_content_home_button);
+        launchUpdateWaterContent.setOnClickListener(this);
+//        ImageView launchViewDailyWaterContent = (ImageView)
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent launchUpdateWaterContentActivity = new Intent(this, UpdateWaterContentActivity.class);
+        startActivity(launchUpdateWaterContentActivity);
     }
 }
