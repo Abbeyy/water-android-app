543d34348e4d3e254f73acd01f1ed30c36024946
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-05-03 15:18:27 +0100

    Somewhat working listviews. Fix needed.

--- app/src/main/java/assessment/c1714546/c1714546assessment/viewWaterContent/ViewDailyWaterContentActivity.java
+++ app/src/main/java/assessment/c1714546/c1714546assessment/viewWaterContent/ViewDailyWaterContentActivity.java
@@ -3,12 +3,14 @@ package assessment.c1714546.c1714546assessment.viewWaterContent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-//import android.support.v7.widget.ListViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,11 +20,14 @@ import assessment.c1714546.c1714546assessment.R;
 import assessment.c1714546.c1714546assessment.updateWaterContent.UpdateWaterContentActivity;
 import assessment.c1714546.c1714546assessment.updateWaterContent.WaterContentRecord;
 
-public class ViewDailyWaterContentActivity extends AppCompatActivity {
+public class ViewDailyWaterContentActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private List<String> entries = new ArrayList<String>();
-//    private ListViewCompat entriesListView;
+    private ListView entriesListView;
     private ArrayAdapter<String> basicArrayAdapter;
     private SharedPreferences numberOfRecords;
+    private SharedPreferences entryClicked;
+    private SharedPreferences.Editor editEntryClicked;
+    private SharedPreferences waterHist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,22 +35,25 @@ public class ViewDailyWaterContentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_daily_water_content);
 
         numberOfRecords = getSharedPreferences("numOfRecs", Context.MODE_PRIVATE);
+        entryClicked = getSharedPreferences("entry", Context.MODE_PRIVATE);
+        editEntryClicked = entryClicked.edit();
+        waterHist = getSharedPreferences("waterHistory", Context.MODE_PRIVATE);
 
         Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         generateList();
-//
-//        this.entriesListView = findViewById(R.id.entries_listview);
-//
-//        this.basicArrayAdapter = new ArrayAdapter<String>(
-//                this,
-//                android.R.layout.simple_list_item_1,
-//                this.entries
-//        );
-//
-//        this.entriesListView.setAdapter(this.basicArrayAdapter);
-//        this.entriesListView.setOnItemClickListener(this);
+
+        this.entriesListView = findViewById(R.id.entries_listview);
+
+        this.basicArrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                this.entries
+        );
+
+        this.entriesListView.setAdapter(this.basicArrayAdapter);
+        this.entriesListView.setOnItemClickListener(this);
     }
 
     public void generateList() {
@@ -63,4 +71,21 @@ public class ViewDailyWaterContentActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String value = entryClicked.getString("record", "error");
+        if ((value != null) && (value != "error")) {
+            editEntryClicked.clear();
+        }
+        String recordNum = Integer.toString(position + 1);
+        editEntryClicked.putString("recordId", recordNum);
+        String glasses = waterHist.getString(recordNum+"glasses", "error");
+        editEntryClicked.putString("glasses", glasses);
+        String time = waterHist.getString(recordNum+"time", "error");
+        editEntryClicked.putString("time", time);
+        editEntryClicked.apply();
+
+        Intent launchViewDailyWaterDetailsActivity = new Intent(this, ViewDailyWaterDetailsActivity.class);
+        startActivity(launchViewDailyWaterDetailsActivity);
+    }
 }
