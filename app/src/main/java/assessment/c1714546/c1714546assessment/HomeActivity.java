02c63edfd9943a146839bd591e68607844fc3c63
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-05-03 11:07:12 +0100

    Launches activities upon action clicks by user from toolbar/menu.

--- app/src/main/java/assessment/c1714546/c1714546assessment/HomeActivity.java
+++ app/src/main/java/assessment/c1714546/c1714546assessment/HomeActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import assessment.c1714546.c1714546assessment.settingsForApp.SettingsForUserActivity;
 import assessment.c1714546.c1714546assessment.tipsAboutWaterContent.TipsAboutDrinkingWaterActivity;
@@ -65,6 +67,36 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int actionID = item.getItemId();
+
+        switch (actionID) {
+            case R.id.home_activity_icon :
+                Toast.makeText(this, "Remaining on Home page...", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.update_water_icon :
+                Intent launchUpdateWaterContentActivity = new Intent(this, UpdateWaterContentActivity.class);
+                startActivity(launchUpdateWaterContentActivity);
+                break;
+            case R.id.view_water_icon :
+                Intent launchViewDailyWaterContentActivity = new Intent(this, ViewDailyWaterContentActivity.class);
+                startActivity(launchViewDailyWaterContentActivity);
+                break;
+            case R.id.tips_icon :
+                Intent launchTipsAboutWaterContentActivity = new Intent(this, TipsAboutDrinkingWaterActivity.class);
+                startActivity(launchTipsAboutWaterContentActivity);
+                break;
+            case R.id.settings_icon :
+                Intent launchSettingsForUserActivity = new Intent(this, SettingsForUserActivity.class);
+                startActivity(launchSettingsForUserActivity);
+                break;
+            default :
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
