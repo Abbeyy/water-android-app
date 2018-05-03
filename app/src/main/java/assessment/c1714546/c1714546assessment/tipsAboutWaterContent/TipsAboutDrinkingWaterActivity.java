aa15853aea9feee387e9a2c0808e8f293f37f59b
Abbey Ross <RossA9@Cardiff.ac.uk>
2018-05-03 14:59:47 +0100

    Added and implemented a fragment. Working on switching it to the next.

--- app/src/main/java/assessment/c1714546/c1714546assessment/tipsAboutWaterContent/TipsAboutDrinkingWaterActivity.java
+++ app/src/main/java/assessment/c1714546/c1714546assessment/tipsAboutWaterContent/TipsAboutDrinkingWaterActivity.java
@@ -1,15 +1,23 @@
 package assessment.c1714546.c1714546assessment.tipsAboutWaterContent;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import assessment.c1714546.c1714546assessment.R;
 
 public class TipsAboutDrinkingWaterActivity extends AppCompatActivity {
+    private FragmentManager fragManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips_about_drinking_water);
+
+        this.fragManager = getSupportFragmentManager();
+        this.fragmentTransaction = this.fragManager.beginTransaction();
+        this.fragmentTransaction.add(R.id.tips_fragment_container, new TipsFragmentOne()).commit();
     }
 }
---
+++ app/src/main/java/assessment/c1714546/c1714546assessment/tipsAboutWaterContent/TipsFragmentTwo.java
@@ -0,0 +1,31 @@
+package assessment.c1714546.c1714546assessment.tipsAboutWaterContent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import assessment.c1714546.c1714546assessment.R;
+
+/**
+ * Created by c1714546 on 5/3/2018.
+ */
+
+public class TipsFragmentTwo extends Fragment {
+
+    public TipsFragmentTwo() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tips_fragment_two_layout, container, false);
+
+
+        return v;
+    }
+
+}
